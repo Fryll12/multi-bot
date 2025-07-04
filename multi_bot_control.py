ace82c9d13313d428258e60417cc216ec33bc8f3
Fryll <nhat48350@gmail.com>
2025-07-04 16:56:46 +0700

    lan

--- multi_bot_control.py
+++ multi_bot_control.py
@@ -30,7 +30,7 @@ heart_threshold_2 = 50
 last_drop_msg_id = ""
 acc_names = [
     "Blacklist", "Khanh bang", "Dersale", "Venus", "WhyK", "Tan",
-    "Ylang", "Nina", "Nathan", "Ofer", "White", "UN the Wicker", "sly_dd"
+    "Ylang", "Nina", "Nathan", "Ofer", "White", "UN the Wicker", "Leader", "Tess", "Wyatt", "Daisy", "CantStop", "Silent", "sly_dd"
 ]
 
 spam_enabled = False
